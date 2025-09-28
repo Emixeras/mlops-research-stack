e77a920436307055c41df998923131f6e127f2dc
Felix Hendrik Josef Hagenbrock <felix_hendrik_josef.hagenbrock@smail.th-koeln.de>
2025-09-28 11:53:53 +0200

    fix the schedules

--- mlops-system-dagster/src/mlops_system_dagster/defs/schedules.py
+++ mlops-system-dagster/src/mlops_system_dagster/defs/schedules.py
@@ -2,7 +2,8 @@ from typing import Union
 
 import dagster as dg
 
+_all_assets_job = dg.define_asset_job("all_assets")
 
-@dg.schedule(cron_schedule="@daily", target="*")
-def schedules(context: dg.ScheduleEvaluationContext) -> Union[dg.RunRequest, dg.SkipReason]:
+@dg.schedule(cron_schedule="@daily", job=_all_assets_job)
+def daily_all(context: dg.ScheduleEvaluationContext) -> Union[dg.RunRequest, dg.SkipReason]:
     return dg.SkipReason("Skipping. Change this to return a RunRequest to launch a run.")
