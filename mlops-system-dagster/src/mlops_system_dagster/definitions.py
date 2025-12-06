9e639fba194789321009b1621186ba10bb9e9523
Felix Hendrik Josef Hagenbrock <felix_hendrik_josef.hagenbrock@smail.th-koeln.de>
2025-12-06 16:33:48 +0100

    fix bug in asset selection

--- mlops-system-dagster/src/mlops_system_dagster/definitions.py
+++ mlops-system-dagster/src/mlops_system_dagster/definitions.py
@@ -4,6 +4,7 @@ from dagster import (
     load_assets_from_modules,
     define_asset_job,
     in_process_executor,
+    AssetSelection,
 )
 
 from .defs import assets, monitoring_assets
@@ -14,7 +15,7 @@ all_assets = load_assets_from_modules([assets, monitoring_assets])
 # Job for drift monitoring with in-process executor (Evidently doesn't work well with multiprocess)
 drift_monitoring_job = define_asset_job(
     name="drift_monitoring",
-    selection=["drift_report"],
+    selection=AssetSelection.assets("drift_report"),
     executor_def=in_process_executor,
 )
 
